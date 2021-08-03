9dd091495c6b9c97437923078084bb836648d8ae
wanjun199510 <81563446+wanjun199510@users.noreply.github.com>
2021-08-03 16:14:28 +0800

    Update cluster_clock_gating.sv

--- rtl/components/functional/cluster_clock_gating.sv
+++ rtl/components/functional/cluster_clock_gating.sv
@@ -16,10 +16,6 @@ module cluster_clock_gating
     output logic clk_o
   );
 
-`ifdef PULP_FPGA_EMUL
-  // no clock gates in FPGA flow
-  assign clk_o = clk_i;
-`else
   logic clk_en;
 
   always_latch
@@ -29,6 +25,5 @@ module cluster_clock_gating
   end
 
   assign clk_o = clk_i & clk_en;
-`endif
 
 endmodule
